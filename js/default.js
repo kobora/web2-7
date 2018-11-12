5639892f7c97c43252f04d45ddb8cc7067326990
kobora <32862953+kobora@users.noreply.github.com>
2018-11-12 16:59:04 +0900

    Update and rename css/default.css to js/default.js

---
+++ js/default.js
@@ -0,0 +1,15 @@
+document.getElementById(`choice`).textContent=
+var result = window.confirm(``);
+document.getElementById(`choice`).textContent=result
+if(result){
+ var nunber
+ var answer
+ 
+ var message;
+ if(answer === nunber
+ message
+}else if (answer < nunber){
+ message =
+ }else if
+ message =
+}
