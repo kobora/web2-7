c1f4d46e1a3c14d06c82c8f4f31a733822ad6ecf
kobora <32862953+kobora@users.noreply.github.com>
2018-11-12 17:55:03 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -6,9 +6,9 @@
  if(answer === number){
  message = `あたり!`;
 }else if (answer < nunber){
- message =`残念！もっと大きいです`;
+ message = `残念！もっと大きいです`;
  }else if(number < answer){
- message =`残念!もっと小さいです`;
+ message = `残念!もっと小さいです`;
 }else{
  message = `0~3の数字を入力してください`;
 }
