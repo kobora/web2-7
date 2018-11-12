752af186c98d5c0b6882c168f1de31ce7e3526fc
kobora <32862953+kobora@users.noreply.github.com>
2018-11-12 18:08:05 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,16 +1,16 @@
 
  var nunber = Math.floor(Math.random()*3);
- var answer = parseInt(window.prompt(`数あてゲーム。０～２の数字を入力してください！`));
+ var answer = parseInt(window.prompt("数あてゲーム。０～２の数字を入力してください！"));
  
  var message;
  if(answer === number){
- message = `あたり!`;
+ message = "あたり!";
 }else if (answer < nunber){
- message = `残念！もっと大きいです`;
+ message = "残念！もっと大きいです";
  }else if(number < answer){
- message = `残念!もっと小さいです`;
+ message = "残念!もっと小さいです";
 }else{
- message = `0~3の数字を入力してください`;
+ message = "0~3の数字を入力してください";
 }
 
 document.getElementById(`choice`).textContent = message;
