2c0ef931ca894fdbf3bde3dddac1cab0e929d077
kobora <32862953+kobora@users.noreply.github.com>
2018-11-12 17:32:59 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,15 +1,16 @@
-document.getElementById(`choice`).textContent=
-var result = window.confirm(``);
-document.getElementById(`choice`).textContent=result
-if(result){
+
  var nunber
  var answer
  
  var message;
- if(answer === nunber
- message
+ if(answer === nunber){
+ message = `あたり!`;
 }else if (answer < nunber){
- message =
- }else if
- message =
+ message =`残念！もっと大きいです`
+ }else if(number < answer){
+ message =`残念!もっと小さいです`;
+}else{
+ message = `0~3`;
 }
+
+document.getElementById(`choice`).textContent = message;
